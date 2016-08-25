dd3db61a6c311594fa925ac3c6f465c3786b9f1f
cbreto <cbreto@umich.edu>
2016-08-25 10:55:07 -0400

    - fixed inconsistent output of panel_logmeanexp depending on whether se=T or F

--- R/misc-exported.R
+++ R/misc-exported.R
@@ -6,7 +6,7 @@ NULL
 #'
 #' Log-mean-exp for panels.
 #'
-#' When \code{se = TRUE}, the jackknife se's from \code{logmeanexp} are squared and summed.
+#' When \code{se = TRUE}, the jackknife se's from \code{logmeanexp} are squared, summed and the squared root is taken.
 #'
 #' @param x Matrix with the same number of replicated estimates for each panel unit loglikelihood.
 #' @param MARGIN The dimension of the matrix that corresponds to a panel unit and over which averaging occurs (1 indicates rows, 2 indicates columns).
@@ -14,15 +14,15 @@ NULL
 #' 
 #' @export
 #'
-panel_logmeanexp <- function (x, MARGIN, se = FALSE) 
+panel_logmeanexp <- function (x, MARGIN, se = FALSE)
 {
-  logmeanexps <- apply(X = x, MARGIN = MARGIN, FUN = logmeanexp, se = TRUE)
-  if (se) {
-    c(sum(logmeanexps[1,]),
-      se = sum((unname(logmeanexps[2,]))^2)^(1/2))
+  logmeanexps <-apply(X = x, MARGIN = MARGIN, FUN = logmeanexp, se = TRUE)
+  summed <- sum(logmeanexps[1, ])
+  if (!se) {
+    summed
   } else {
-    logmeanexps
-  }
+    c(summed, se = sqrt(sum(unname(logmeanexps[2, ]) ^ 2)))
+    }
 }
 
 
