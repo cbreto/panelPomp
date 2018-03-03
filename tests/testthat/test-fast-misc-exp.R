444e3a849f3523e67e3b55dcfe3dfe8a00afe872
cbreto <cbreto@umich.edu>
2018-03-03 16:47:33 -0500

    Add get_col_row tests

--- tests/testthat/test-fast-misc-exp.R
+++ tests/testthat/test-fast-misc-exp.R
@@ -39,29 +39,3 @@ test_that("to- and fromVectorPparams don't match (with both shared & specific pa
   res <- panelPomp::fromVectorPparams(vectorpParams)
   expect_true(identical(res,listPparams))
 })
-
-
-context("Test get_col")
-
-test_that("get_col fails to keep name in 2x5 matrix", {
-  shared <- c(shared.1=1,shared.2=2)
-  u <- 5 # at least 1
-  mat <- matrix(
-    as.numeric(paste0(rep(seq(3,u+2),each=2),rep(c(".1",".2"),times=u))),
-    nrow=2,dimnames=list(c("spec.1","spec.2"),c(paste0("unit.",1:u)))
-  )
-  col <- 1
-  rows <- 1
-  x <- get_col(mat,rows,col)
-  y <- setNames(mat[1,1],nm=rownames(mat)[rows])
-  expect_true(identical(x,y))
-})
-
-test_that("get_col fails to keep name in 1x1 matrix", {
-  matrix(1,dimnames=list("spec.1","unit.1")) -> mat
-  col <- 1
-  rows <- 1
-  get_col(mat,rows,col) -> x
-  setNames(mat[1,1],nm=rownames(mat)[rows]) -> y
-  expect_true(identical(x,y))
-})
