0eece7faba840467ca97063b763eb789e1c3a19c
cbreto <cbreto@umich.edu>
2018-03-06 15:29:32 -0500

    Update test results

--- R/aaa.R
+++ R/aaa.R
@@ -14,31 +14,25 @@ NULL
   options(pomp.examples=exampleDir)
 }
 
-test <- function (expr1, expr2, all, env, verbose = TRUE, ...) {
-  # expr1: expression to be eval()d (if missing expr2)
-  # expr2: optional; compare to expr1 (via identical(try(expr1),try(expr2)))
+test <- function (expr1, expr2, all, env, verbose = TRUE) {
+  # expr1: expression to be try(,sil=T)[1]d
+  # expr2: optional; compare to 'expr1' (via identical) and c() logical result
+  #     to object specified via arguments 'all' and 'env' (see below)
   # all: name of the vector to accumulate logical test results
   # env: name of the environment where 'all' should be modified
-  # verbose: optional; should the test output be returned?
-  # ...: arguments to be passed to eval()
+  # verbose: optional; should the result be returned?
   ep <- paste0("in ",sQuote("test"),": ")
   if (!exists(all,envir=env))
     stop(paste0(ep,"missing vector to accumulate logical ",
                 "test results."),call.=FALSE)
-  if (missing(expr2)) {
-    expr <- expression(expr1)
-  } else {
-    tryexpr1 <- try(expr1,silent=TRUE)
-    if (is(tryexpr1,"try-error")) tryexpr1 <- tryexpr1[1]
+  tryexpr1 <- try(expr1,silent=TRUE)
+  if (is(tryexpr1,"try-error")) tryexpr1 <- tryexpr1[1]
+  PASSES <- tryexpr1
+  if (!missing(expr2)) {
     tryexpr2 <- try(expr2,silent=TRUE)
     if (is(tryexpr2,"try-error")) tryexpr2 <- tryexpr2[1]
-    expr <- expression(identical(tryexpr1,tryexpr2))
-    }
-  PASSES <- tryCatch(eval(expr,...),
-                     error=function (e) {stop(ep,conditionMessage(e),
-                                              call.=FALSE)})
-  if (!is.logical(PASSES))
-    stop(paste0("in ",sQuote("test"),": non-logical test result!"),call.=FALSE)
-  assign(all,value=c(get(all,envir=env),PASSES),envir=env)
+    PASSES <- identical(tryexpr1,tryexpr2)
+    assign(all,value=c(get(all,envir=env),PASSES),envir=env)
+  }
   if (verbose) PASSES
 }
--- tests/aaa.R
+++ tests/aaa.R
@@ -6,45 +6,36 @@ TESTS_PASS <- NULL
 test <- function(expr1,expr2,all="TESTS_PASS",env=parent.frame(),...) 
   panelPomp:::test(expr1,expr2,all=all,env=env,...)
 ## ..., perform tests, and ...
-test(identical(NULL,NULL))
+test(NULL,NULL)
 ## check whether all tests passed
 all(get(eval(formals(test))$all))
 if (!all(get(eval(formals(test))$all))) stop("Not all tests passed!")
 
-
-## refresh <- expression({
-##   rm(list=ls()[!(ls() %in% c('test','TESTS_PASS','refresh'))])
-##   refreshed <- NULL
-## })
-## eval(refresh)
-
-
 ## complete tests for 'test':
 ## test if results are added to 'all'
 test(NULL,NULL)
+## if only one argument, try(,silent=T)[1] 
+## (not necessarily logical; hence, all is not changed!)
 test(length(get(eval(formals(test))$all))==2)
-test(length(get(eval(formals(test))$all)),3L)
+test(length(get(eval(formals(test))$all)),2L)
 ## test order of expr1 and expr2
-test(4L,length(get(eval(formals(test))$all)))
+test(3L,length(get(eval(formals(test))$all)))
 ## test stop for wrong parameters
-test(wQuotes("Error : in ''test'': object 'invalid_expr' not found\n"),
-     test(invalid_expr))
-test(wQuotes("Error : in ''test'': non-logical test result!\n"),test(NULL))
+test(wQuotes("Error in try(expr1, silent = TRUE) : object 'invalid_expr' not ",
+             "found\n"),test(invalid_expr))
 test(wQuotes("Error : in ''test'': missing vector to accumulate logical test ",
              "results.\n"),
-     panelPomp:::test(identical(NULL,NULL),all="wrong_all",env=parent.frame()))
+     panelPomp:::test(NULL,expr2=NULL,all="wrong_all",env=parent.frame()))
 test("Error in exists(all, envir = env) : invalid 'envir' argument\n",
-     panelPomp:::test(
-       identical(NULL,NULL),all=eval(formals(test))$all,env="no_env"))
+     panelPomp:::test(NULL,NULL,all=eval(formals(test))$all,env="no_env"))
 ## test identical for range of objects
 test(NA,NA)
 test(1,1)
 test(1L,1L)
 test("a","a")
-test(matrix(1,nrow=2) -> m,m)
-test(list(a="a",b="b") -> l,l)
-test(c(TRUE,TRUE) -> L,L)
-test(wQuotes("Error : in ''test'': object 'not_found' not found\n"),test(not_found))
+test(matrix(1,nrow=2),matrix(1,nrow=2))
+test(list(a="a",b="b"),list(a="a",b="b"))
+test(c(TRUE,TRUE),c(TRUE,TRUE))
 ## tests for .onAttach
 test(tail(strsplit(options("pomp.examples")$pomp.examples[2],"/")[[1]],2),
      c("panelPomp","examples"))
