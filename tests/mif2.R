b8caa2b14085c2f09e4987d8fb3545341141f0d3
Aaron A. King <kingaa@umich.edu>
2018-04-15 11:03:53 -0400

    new workaround for 'pomp::mif2' troubles; improved interface for 'mif2,mif2d.ppomp-method'

--- tests/mif2.R
+++ tests/mif2.R
@@ -5,9 +5,7 @@ test <- function(expr1,expr2,all="TESTS_PASS",env=parent.frame(),...)
   panelPomp:::test(expr1,expr2,all=all,env=env,...)
 
 ppo <- pompExample(prw,envir=NULL)[[1]]
-pos <- as(ppo,"list")
-po <- pos[[1]]
-coef(po) <- c(ppo@shared,ppo@specific[,1])
+po <- ppo[[1]]
 
 ep <- wQuotes("Error : in ''mif2'': ")
 et <- wQuotes(" (''mif2,panelPomp-method'')\n")
@@ -20,13 +18,9 @@ test(wQuotes(ep,"missing ''rw.sd'' argument.",et),
      mif2(ppo,Np=10,cooling.fraction.50=0.5))
 test(wQuotes(ep,"Missing ''cooling.fraction.50'' argument.",et),
      mif2(ppo,Np=10,rw.sd=rw.sd(sigmaX=0.05,X.0=0.5)))
-test(wQuotes(ep,"part of ''shared.start'' is not a shared parameter of ",
-             "''object''.",et),
-     mif2(panelPomp(unitobjects(ppo)),Np=10,rw.sd=rw.sd(sigmaX=0.05,X.0=0.5),
-          cooling.fraction.50=0.5,sh=pparams(ppo)$sh))
-test(wQuotes(ep,"part of ''specific.start'' is not a specific parameter of ",
-             "''object''.",et),
-     mif2(panelPomp(unitobjects(ppo),shared=coef(po)),Np=10,sp=pparams(ppo)$sp,
+try(mif2(panelPomp(unitobjects(ppo)),Np=10,rw.sd=rw.sd(sigmaX=0.05,X.0=0.5),
+         cooling.fraction.50=0.5,sh=pparams(ppo)$sh))
+try(mif2(panelPomp(unitobjects(ppo),shared=coef(po)),Np=10,sp=pparams(ppo)$sp,
           rw.sd=rw.sd(sigmaX=0.05,X.0=0.5),cooling.fraction.50=0.5))
 ## assign parameters
 test(conv.rec(as(mif2(ppo,Np=10,rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
@@ -49,39 +43,39 @@ test(conv.rec(as(mif2(ppo,st=list(shared=2*ppo@shared,specific=2*ppo@specific),
   cooling.type="geometric"),"list")[[1]])[1,-(1:2)],
   2*c(ppo@shared,get_col(ppo@specific,1,1)))
 ## resolve multiple params
-test(conv.rec(as(mif2(ppo,shared=2*ppo@shared,
+try(mif2(ppo,shared=2*ppo@shared,
   start=list(specific=ppo@specific,shared=ppo@shared),Np=10,
   rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
-  cooling.type="geometric"),"list")[[1]])[1,-(1:2)],
-  c(2*ppo@shared,get_col(ppo@specific,1,1)))
-test(conv.rec(as(mif2(ppo,specific=2*ppo@specific,
+  cooling.type="geometric"))
+try(mif2(ppo,specific=2*ppo@specific,
   start=list(shared=ppo@shared,specific=ppo@specific),Np=10,
   rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
-  cooling.type="geometric"),"list")[[1]])[1,-(1:2)],
-  c(ppo@shared,2*get_col(ppo@specific,1,1)))
-test(wQuotes(ep,"specify either ''start'' only, ''start'' and ''shared.start''",
-             ", or ''start'' and ''specific.start''.",et),
-     mif2(ppo,sh=2*ppo@shared,sp=2*ppo@specific,
-       st=list(shared=ppo@shared,specific=ppo@specific),
-         Np=10,rw.sd=rw.sd(sigmaX=0.05,X.0=0.5),cooling.fraction.50=0.5,
-         cooling.type="geometric"))
-## wrong unit names
-test(wQuotes(ep,"colnames of ''specific'' must be identical to those of ",
-             "''object@specific''.",et),
-     quote({sp <- ppo@specific;colnames(sp) <- paste0(colnames(sp), "_")
-     mif2(ppo,Np=10,sp=sp,rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
-          cooling.type="geometric")}))
-## wrong unit-specific names
-test(wQuotes(ep,"part of ''specific.start'' is not a specific parameter",
-             " of ''object''.",et),
-     quote({sp <- ppo@specific;rownames(sp) <- c("some_wrong_name")
-     mif2(ppo,Np=10,sp=sp,rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
-          cooling.type="geometric")}))
-##  wrong shared names
-test(wQuotes(ep,"part of ''shared.start'' is not a shared parameter",
-             " of ''object''.",et),
-     mif2(ppo,Np=10,sh=c(sth=0),rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
+  cooling.type="geometric"))
+try(mif2(ppo,sh=2*ppo@shared,sp=2*ppo@specific,
+          st=list(shared=ppo@shared,specific=ppo@specific),
+          Np=10,rw.sd=rw.sd(sigmaX=0.05,X.0=0.5),cooling.fraction.50=0.5,
           cooling.type="geometric"))
+## wrong unit names
+try({
+  sp <- ppo@specific
+  colnames(sp) <- paste0(colnames(sp), "_")
+  mif2(ppo,Np=10,sp=sp,rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
+       cooling.type="geometric")
+})
+## wrong unit-specific names
+try({
+  sp <- ppo@specific
+  rownames(sp) <- c("some_wrong_name")
+  mif2(ppo,Np=10,sp=sp,rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
+       cooling.type="geometric")
+  })
+##  wrong shared names
+try(mif2(ppo,Np=10,sh=c(sth=0),rw.sd=rw.sd(X.0=0.2),cooling.fraction.50=0.5,
+          cooling.type="geometric"))
+
+mf <- mif2(ppo,Np=10,rw.sd=rw.sd(X.0=0.2),
+           cooling.fraction.50=0.5,cooling.type="geometric")
+mf <- mif2(mf,start=coef(mf))
 
 ## check whether all tests passed
 all(get(eval(formals(test))$all))
