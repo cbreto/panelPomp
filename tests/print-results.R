cedd194e4bb3ab06f90590e988b7888793363eca
cbreto <cbreto@umich.edu>
2019-04-23 13:08:29 +0200

    Change dependence to pomp2 (RCMDch)

--- tests/print-results.R
+++
@@ -1,8 +0,0 @@
-library(panelPomp,quietly=TRUE)
-if (file.exists("options.R")) source("options.R")
-
-## list examples (avoiding listing package installation folder for R CMD check)
-capture.output(panelPompExample())[2]
-ppo <- panelPompExample("prw")
-ppo
-lapply(as(ppo,"list"),obs)
