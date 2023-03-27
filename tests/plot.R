eadd6a0b17944b2647a487124f80aa8a5848c708
cbreto <cbreto@umich.edu>
2023-03-27 19:30:29 +0200

    improve 'tests/plot.R'

--- tests/plot.R
+++ tests/plot.R
@@ -1,8 +1,9 @@
 if (file.exists("_options.R")) source("_options.R")
 library(panelPomp,quietly=TRUE)
 
-options(digits=3)
-png(filename="plot-%02d.png",res=100)
+old_o <- options(digits=3)
+png(file.path(tempdir(),"plot-%02d.png"),res=100)
 ppo <- panelPomp:::panelRandomWalk(N=5,U=2)
 plot(ppo)
 dev.off()
+options(old_o)
