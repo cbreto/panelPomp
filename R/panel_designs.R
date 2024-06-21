102258e90f2c7e0ed090217fe1337a8368668d10
Jesse Wheeler PC <jeswheel@umich.edu>
2024-06-21 14:26:10 -0400

    Added runif_panel_design

---
+++ R/panel_designs.R
@@ -0,0 +1,107 @@
+#' #' Create design matrix for panelPomp calculations
+#'
+#' These functions are useful for generating design matrices for the exploration
+#' of parameter space.
+#'
+#' @name panel-designs
+#' @concept panel-designs
+#'
+#' @author Jesse Wheeler, Aaron A. King
+NULL
+
+#' \code{runif_panel_design} generates a design based on random samples from a
+#' multivariate uniform distribution.
+#'
+#' @param lower,upper named numeric vectors giving the lower and upper bounds
+#'    of the ranges, respectively.
+#' @param nseq Total number of points requested
+#' @param specific_names Character vector containing the names of unit-specific
+#'    parameters. This argument must be used in conjunction with the argument
+#'    \code{unit_names}; it is used if the search bounds for all unspecified
+#'    unit-specific parameters are the same.
+#' @param unit_names Character vector containing the names of the units of the
+#'    panel. If not used in conjunction with \code{unit_names} this argument
+#'    is ignored.
+#'
+#' @return \code{runif_panel_design} returns a \code{data.frame} object with
+#'    \code{nseq} rows. Each row corresponds to a parameter set drawn randomly
+#'    from a multivariate uniform distribution specified by the \code{lower},
+#'    \code{upper}, \code{specific_names} and \code{unit_names} arguments.
+#' @export
+#'
+#' @examples
+#' runif_panel_design(
+#'   lower = c('a' = 0, 'b' = 10, 'a[u2]' = 0.5),
+#'   upper = c('a' = 1, 'b' = 15, 'a[u2]' = 0.75),
+#'   specific_names = c('a'),
+#'   unit_names = paste0(rep('u', 5), 1:5),
+#'   nseq = 10
+#' )
+#' @rdname panel-designs
+runif_panel_design <- function (
+    lower = numeric(0), upper = numeric(0), nseq, specific_names, unit_names
+) {
+
+  # Checks made in pomp::runif_design
+  if (length(lower)!=length(upper))
+    pStop(sQuote("lower")," and ",sQuote("upper")," must have same length.")
+  lnames <- names(lower)
+  if (is.null(lnames))
+    pStop(sQuote("lower")," and ",sQuote("upper")," must be named vectors.")
+  if (!all(sort(lnames)==sort(names(upper))))
+    pStop("names of ",sQuote("lower")," and ",sQuote("upper")," must match.")
+  upper <- upper[lnames]
+  if (!all(upper>=lower))
+    pStop("upper values should be at least as large as lower ones.")
+  nseq <- as.integer(nseq)
+  if (nseq < 0)
+    pStop(sQuote("nseq"),"< 0.")
+
+  if (missing(specific_names) && missing(unit_names)) {
+    # Do as pomp::runif_design
+    y <- matrix(
+      data=runif(n=nseq*length(lower),min=lower,max=upper),
+      nrow=nseq,ncol=length(lower),
+      byrow=TRUE
+    )
+    colnames(y) <- lnames
+    return(as.data.frame(y))
+  } else {
+    if (missing(specific_names) | missing(unit_names))
+      pStop("If used, both ", sQuote("specific_names")," and ",sQuote("unit_names")," must be provided.")
+
+    if (any(!specific_names %in% lnames))
+      pStop("No bounds were given for some parameters in ", sQuote("specific_names"), ".")
+
+    lwr_tmp <- lower[!lnames %in% specific_names]
+    upr_tmp <- upper[!lnames %in% specific_names]
+    upr_tmp <- upr_tmp[names(lwr_tmp)]
+
+    lwr_spec <- rep(
+      lower[lnames %in% specific_names], each = length(unit_names)
+    )
+
+    unit_labs <- paste0("[", rep(unit_names, length(unique(names(lwr_spec)))), "]")
+    names(lwr_spec) <- paste0(names(lwr_spec), unit_labs)
+
+    upr_spec <- rep(
+      upper[lnames %in% specific_names], each = length(unit_names)
+    )
+
+    names(upr_spec) <- names(lwr_spec)
+
+    lower <- c(lwr_tmp, lwr_spec[!names(lwr_spec) %in% names(lwr_tmp)])
+    upper <- c(upr_tmp, upr_spec[!names(lwr_spec) %in% names(lwr_tmp)])
+
+    lower <- lower[sort(names(lower))]
+    upper <- upper[sort(names(upper))]
+
+    y <- matrix(
+      data=runif(n=nseq*length(lower),min=lower,max=upper),
+      nrow=nseq,ncol=length(lower),
+      byrow=TRUE
+    )
+    colnames(y) <- names(lower)
+    return(as.data.frame(y))
+  }
+}
